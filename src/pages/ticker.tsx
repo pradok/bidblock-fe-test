3561c2a8d4bbd18ba842f8cd19ea518c4c50e0e8
Pradosh Sinha <pradoshks@outlook.com>
2018-07-16 15:26:12 +1000

    Add back button.

--- src/pages/ticker.tsx
+++ src/pages/ticker.tsx
@@ -1,14 +1,22 @@
 import * as React from 'react';
-import { RouteComponentProps } from 'react-router-dom';
+import {RouteComponentProps} from 'react-router-dom';
+import {Button, Icon} from 'antd';
 
 import * as CryptoPairings from '../features/crypto-pairings';
 
-const Ticker = (props: RouteComponentProps<{ index: string, symbol: string }>) => (
-  <React.Fragment>
-    <CryptoPairings.components.CryptoPairingDetails
-      tickerIndex={props.match.params.index}
-      tickerSymbol={props.match.params.symbol}
-    />
-  </React.Fragment>
-);
+const Ticker = (props: RouteComponentProps<{ index: string, symbol: string }>) => {
+  const clickHandler = () => {
+    props.history.push('/');
+  };
+  return (<React.Fragment>
+      <Button type="primary" onClick={clickHandler}>
+        <Icon type="left"/>Go back
+      </Button>
+      <CryptoPairings.components.CryptoPairingDetails
+        tickerIndex={props.match.params.index}
+        tickerSymbol={props.match.params.symbol}
+      />
+    </React.Fragment>
+  )
+};
 export default Ticker;
